bf0f42877fb38dd881d9b447258c0478e852d7d4
Daniel <pc@uni.com>
2024-05-15 19:27:57 -0500

    car-inventory-microservice

---
+++ car-inventory/src/test/java/com/unimagdalena/carinventory/CarInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unimagdalena.carinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
